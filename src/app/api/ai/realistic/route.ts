ab042d3152314838b6f070e860af9e99e9b3d2a9
Raj <blcc.raj@gmail.com>
2024-01-01 01:29:21 +0530

    idk: pushing for to show rohit

---
+++ src/app/api/ai/realistic/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+  },
+];
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+const generationConfig = {
+  temperature: 0.9,
+  topK: 1,
+  topP: 1,
+  maxOutputTokens: 2048,
+};
+export async function POST(request: NextRequest) {
+  const { prompt } = await request.json();
+  const parts = [
+    {
+      text: `Make resolutions that seem impossible for the New Year, but can actually happen in real life. Keep the goals short and easy to understand. give 10 points only. no big sentences ${prompt}}}`,
+    },
+  ];
+  const result = await model.generateContent({
+    contents: [{ role: "user", parts }],
+    generationConfig,
+    safetySettings,
+  });
+
+  const response = result.response;
+  const aiResponse = response.text();
+  console.log(response.text());
+  return NextResponse.json(aiResponse);
+}
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,11 +1,38 @@
+'use client'
+import axios from 'axios';
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
+interface FormData {
+    about: string,
+    goal: string
+
+}
+
+const fetcher = url => fetch(url).then(res => res.json());
 const Create = () => {
+    const { register, handleSubmit, watch } = useForm<FormData>(
+        {
+            defaultValues: {
+                goal: 'realistic'
+            }
+        }
+    );
+
+    const onSubmit = async (data: FormData) => {
+        try {
+            const response = await axios.post('/api/ai/realistic', { prompt: data.about });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
         <div className='bg-secondary flex custom-h items-center justify-center'>
-            <form className='bg-secondary rounded-lg flex flex-col items-center p-4 md:p-8'>
+            <form onSubmit={handleSubmit(onSubmit)} className='bg-secondary rounded-lg flex flex-col items-center p-4 md:p-8'>
                 <div className='bg-secondary-light py-5 px-4 md:px-8 rounded-lg'>
                     <input
+                        {...register("about")}
                         type="text"
                         className='w-full md:w-[590px] h-24 md:h-[171px] rounded-lg text-center'
                         placeholder="Tell us about yourself or what you want to improve in this year?"
@@ -13,36 +40,38 @@ const Create = () => {
                     <p className='text-center text-white font-normal py-2'>Set Your Goal:</p>
                     <div className="flex flex-col md:flex-row justify-center md:space-x-4">
                         <input
+                            {...register("goal")}
                             type="radio"
                             id="realistic"
-                            name="options"
+                            name="goal"
                             value="realistic"
                             className="hidden"
                         />
                         <label
                             htmlFor="realistic"
-                            className="cursor-pointer px-4 py-2 mb-2 md:mb-0 bg-white text-black rounded-md"
+                            className={`cursor-pointer px-4 py-2 mb-2 md:mb-0 rounded-md ${watch('goal') === 'realistic' ? 'bg-green-600 text-white' : 'bg-white text-black'}`}
                         >
                             Realistic
                         </label>
 
                         <input
+                            {...register("goal")}
                             type="radio"
                             id="unrealistic"
-                            name="options"
+                            name="goal"
                             value="unrealistic"
                             className="hidden"
                         />
                         <label
                             htmlFor="unrealistic"
-                            className="cursor-pointer px-4 py-2 bg-white text-black rounded-md"
+                            className={`cursor-pointer px-4 py-2 rounded-md ${watch('goal') === 'unrealistic' ? 'bg-red-600 text-white' : 'bg-white text-black'}`}
                         >
                             Unrealistic
                         </label>
                     </div>
                 </div>
 
-                <button className='w-full md:w-[465px] mt-5 py-2 bg-white text-center text-black rounded-2xl'>
+                <button type="submit" className='w-full md:w-[465px] mt-5 py-2 bg-white text-center text-black rounded-2xl'>
                     Generate
                 </button>
             </form>
