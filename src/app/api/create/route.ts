69e7b9240cae1254cb03ad467a6bac0d6d5affb6
Raj <blcc.raj@gmail.com>
2024-01-02 22:23:46 +0530

    Add DELETE route and handle resolution deletion

--- src/app/api/create/route.ts
+++ src/app/api/create/route.ts
@@ -19,3 +19,42 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const resolution = await db.resolution.findUnique({
+      where: {
+        id: body.id,
+      },
+    });
+
+    if (!resolution) {
+      return NextResponse.json(
+        { message: "Resolution not found!" },
+        { status: 404 }
+      );
+    }
+
+    if (resolution.userId !== body.userId) {
+      return NextResponse.json(
+        { message: "You do not have permission to delete this resolution!" },
+        { status: 403 }
+      );
+    }
+
+    const deletedResolution = await db.resolution.delete({
+      where: {
+        id: body.id,
+      },
+    });
+
+    return NextResponse.json(deletedResolution, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Something went wrong!", error },
+      { status: 500 }
+    );
+  }
+}
--- src/app/resolutions/page.tsx
+++ src/app/resolutions/page.tsx
@@ -3,25 +3,25 @@ import Card from '@/components/Card'
 import { db } from '@/lib/db';
 import { useAuth } from '@clerk/nextjs';
 import axios from 'axios';
-import { ArrowBigRightDash, ArrowRightCircle, DoorOpen, Share } from 'lucide-react';
+import { ArrowBigRightDash, ArrowRightCircle, Delete, DoorOpen, Share, Trash } from 'lucide-react';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
 import Markdown from 'react-markdown';
 
 const MyResolutions = () => {
-    const { isLoaded, userId, sessionId, getToken } = useAuth();
+    const { userId, } = useAuth();
     const [resolutions, setResolutions] = useState<{ id: string; content: string; userId: string; createdAt: Date; updatedAt: Date; }[]>([]);
 
+    const fetchResolutions = async () => {
+        try {
+            const response = await axios.get('/api/my');
+            setResolutions(response.data);
+        } catch (error) {
+            console.error('Error fetching resolutions:', error);
+        }
+    };
     useEffect(() => {
-        const fetchResolutions = async () => {
-            try {
-                const response = await axios.get('/api/my');
-                setResolutions(response.data);
-            } catch (error) {
-                console.error('Error fetching resolutions:', error);
-            }
-        };
 
         fetchResolutions();
     }, [userId]);
@@ -29,6 +29,7 @@ const MyResolutions = () => {
 
         try {
             await navigator.clipboard.writeText(`https://nyn.rajcrafts.tech/resolutions/${id}`);
+            setResolutions(prevResolutions => prevResolutions.filter(resolution => resolution.id !== id));
             toast.success('Copied to clipboard!');
         } catch (err: any) {
             toast.error('Failed to copy text: ', err);
@@ -36,6 +37,21 @@ const MyResolutions = () => {
         }
     };
 
+    const handleDelete = async (id: any) => {
+        try {
+            const resolution = await axios.delete('/api/create', {
+                data: {
+                    id: id,
+                    userId: userId,
+                },
+            });
+            fetchResolutions();
+            toast.success('Resolution deleted successfully');
+        } catch (error: any) {
+            console.error('Error deleting resolution:', error);
+            toast.error('Error deleting resolution', error);
+        }
+    };
 
     return (
         <div className={`gap-5 py-2 px-2  masonry`} >
@@ -53,6 +69,9 @@ const MyResolutions = () => {
 
                         <button className='' onClick={() => copyToClipboard(resolution.id)}>
                             <Share className='text-primary' />
+                        </button>
+                        <button className='' onClick={() => handleDelete(resolution.id)}>
+                            <Trash className='text-red-500 hover:text-red-700' />
                         </button>
 
                     </div>
