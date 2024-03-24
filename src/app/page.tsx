716f7eed96cd4b1946be3eca691aa2c47f8d92fe
Raj <blcc.raj@gmail.com>
2024-03-24 20:03:43 +0530

    fix tagline size on xs devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className='bg-white flex custom-h items-center justify-center'>
       <div className='text-center'>
-        <p className='text-lg text-white w-fit mx-auto px-4 rounded-md mb-8 bg-gradient-to-r from-rose-400 to-orange-300'>
+        <p className='md:text-lg text-white w-fit mx-auto px-4 rounded-full mb-4 bg-gradient-to-r from-rose-400 to-orange-300'>
           Resolutions: Unboring them since 2023
         </p>
         <h1 className='text-2xl md:text-6xl font-bold text-secondary mb-8'>
